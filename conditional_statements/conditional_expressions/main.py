c49315b42b47b24f5fcada6df07ac57582a122c9
spence153 <>
2026-02-06 18:53:19 +0000

    Conditional expressions

---
+++ conditional_statements/conditional_expressions/main.py
@@ -0,0 +1,10 @@
+# Input variables
+product_type = "Vegetables"
+day_of_week = "Monday"
+
+if product_type == "Fruits" and day_of_week == "Monday":
+   print("Fruits discount!")
+elif product_type == "Vegetables" and day_of_week == "Tuesday":
+    print("Vegetables discount!")
+else:
+    print("No discount.")
